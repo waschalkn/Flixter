eafbdc12fbf511ba84bae52d09a5862ee1ddf8b3
waschalkn <nacodaw93@gmail.com>
2019-09-08 23:46:50 +0000

    added carrierwave-aws

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-    storage :fog
+    storage :aws
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -22,7 +22,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-    process resize_to_fill: [200, 300]
+    process resize_to_fill: [800, 350]
   #
   # def scale(width, height)
   #   # do something
