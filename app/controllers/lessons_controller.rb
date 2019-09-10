d9624927ee1fc7375012b365379cbd030f628066
waschalkn <nacodaw93@gmail.com>
2019-09-10 06:15:19 +0000

    bug fixes

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,12 +1,24 @@
 class LessonsController < ApplicationController
+before_action :authenticate_user!
+before_action :require_authorized_for_current_lesson, only: [:show]
 
   def show 
-
+    
   end
 
   private
 
+  def require_authorized_for_current_lesson
+    if current_user.enrolled_in?(@course) != current_lesson.section.course
+      redirect_to course_path, alert: 'Error Message' 
+    else
+      
+    end
+  end
+
   helper_method :current_lesson
+
+
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
